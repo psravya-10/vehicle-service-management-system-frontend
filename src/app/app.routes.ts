712f01892d2c7c6f2f07fdf5d681a90901cd8dfd
sravya <podilasravya7052@gmail.com>
2026-01-04 14:48:25 +0530

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { Dashboard as CustomerDashboard } from './features/customer/dashboard/dashboard';
+import { Dashboard as TechnicianDashboard } from './features/technician/dashboard/dashboard';
+import { Dashboard as ManagerDashboard } from './features/manager/dashboard/dashboard';
+import { Dashboard as AdminDashboard } from './features/admin/dashboard/dashboard';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'home', redirectTo: '' }
+    { path: 'home', redirectTo: '' },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'customer/dashboard', component: CustomerDashboard, canActivate: [authGuard] },
+    { path: 'technician/dashboard', component: TechnicianDashboard, canActivate: [authGuard] },
+    { path: 'manager/dashboard', component: ManagerDashboard, canActivate: [authGuard] },
+    { path: 'admin/dashboard', component: AdminDashboard, canActivate: [authGuard] }
 ];
